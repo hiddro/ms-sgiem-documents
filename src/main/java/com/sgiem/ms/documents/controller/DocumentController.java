bf523eabfc066f1635c020946e858551b8bf50d4
@EduCCor <edd.ckalb@gmail.com>
2023-07-21 02:08:08 -0500

    added: openapigenerator pom / modified: controller & openapi

--- src/main/java/com/sgiem/ms/documents/controller/DocumentController.java
+++ src/main/java/com/sgiem/ms/documents/controller/DocumentController.java
@@ -4,16 +4,23 @@ import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.sgiem.ms.documents.api.v1.DocumentApi;
+import com.sgiem.ms.documents.dto.FileResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.http.codec.multipart.Part;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.File;
@@ -23,7 +30,8 @@ import java.util.UUID;
 
 @RestController
 @Slf4j
-public class DocumentController {
+@CrossOrigin(origins = "*")
+public class DocumentController implements DocumentApi {
 
     @Value("${blob.connection-string}")
     String connectionString;
@@ -33,36 +41,45 @@ public class DocumentController {
     @Autowired
     WebClient webClient;
 
-    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public Mono<String> uploadFile(@RequestPart("file") FilePart filePart) {
-        String fileName = "prefix-" + UUID.randomUUID().toString() + "-" + filePart.filename();
-        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
-        BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
-        BlobClient blobClient = containerClient.getBlobClient(fileName);
+    @Override
+    public Mono<ResponseEntity<FileResponse>> uploadFile(Flux<Part> file, ServerWebExchange exchange) {
+        return file
+                .ofType(FilePart.class)
+                .flatMap(filePart -> {
+                    String fileName = "sgiemcv-" + UUID.randomUUID().toString() + "-" + filePart.filename();
+                    BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
+                    BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
+                    BlobClient blobClient = containerClient.getBlobClient(fileName);
 
-        File tempFile;
-        try {
-            tempFile = File.createTempFile("temp-", null);
-        } catch (IOException e) {
-            return Mono.error(e);
-        }
+                    File tempFile;
+                    try {
+                        tempFile = File.createTempFile("temp-", null);
+                    } catch (IOException e) {
+                        return Mono.error(e);
+                    }
 
-        Mono<Void> saveFileMono = filePart.transferTo(tempFile);
-
-        return saveFileMono.then(Mono.fromCallable(() -> {
-            blobClient.uploadFromFile(tempFile.getAbsolutePath());
-            return blobClient.getBlobUrl();
-        })).doFinally(signalType -> {
-            try {
-                Files.deleteIfExists(tempFile.toPath());
-            } catch (IOException e) {
-                log.error("Error al crear el archivo temporal", e);
-            }
-        });
+                    return filePart.transferTo(tempFile)
+                            .then(Mono.fromCallable(() -> {
+                                blobClient.uploadFromFile(tempFile.getAbsolutePath());
+                                return blobClient.getBlobUrl();
+                            }))
+                            .doFinally(signalType -> {
+                                try {
+                                    Files.deleteIfExists(tempFile.toPath());
+                                } catch (IOException e) {
+                                    log.error("Error al crear el archivo temporal", e);
+                                }
+                            });
+                })
+                .next()
+                .map(e -> ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(FileResponse.builder().url(e).build())
+                );
     }
 
-    @GetMapping("/download/{documentName}")
-    public Mono<ResponseEntity<byte[]>> downloadDocument(@PathVariable String documentName) {
+    @Override
+    public Mono<ResponseEntity<Resource>> downloadFile(String documentName, ServerWebExchange exchange) {
         return webClient
                 .get()
                 .uri("https://sgiemstorage.blob.core.windows.net/sgiemcontainer/{documentName}", documentName)
@@ -70,10 +87,13 @@ public class DocumentController {
                 .exchangeToMono(response -> {
                     if (response.statusCode().equals(HttpStatus.OK)) {
                         return response.bodyToMono(byte[].class)
-                                .map(content -> ResponseEntity.ok()
-                                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + documentName)
-                                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                                        .body(content));
+                                .map(content -> {
+                                    Resource resource = new ByteArrayResource(content);
+                                    return ResponseEntity.ok()
+                                            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + documentName)
+                                            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                                            .body(resource);
+                                });
                     } else {
                         return Mono.just(ResponseEntity.status(response.statusCode()).build());
                     }
